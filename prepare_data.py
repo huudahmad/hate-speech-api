0425532b2dee8e5555e7914c7018768426a487e8
Huud Ahmad <huudahmad@Mac.lan>
2026-01-14 01:06:09 -0600

    added fix to remove rows that become empty after cleaning to avoid NaN error when training model

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 url = "https://raw.githubusercontent.com/t-davidson/hate-speech-and-offensive-language/master/data/labeled_data.csv"
@@ -16,6 +17,17 @@ def clean_text(text):
 
 print("Cleaning text data")
 df['cleaned_text'] = df['tweet'].apply(clean_text)
+
+#model training fails if there are any empty rows after cleaning because Pandas treats them as NaN (missing data)
+# --- FIX: Drop empty rows (Updated to avoid warning) ---
+# Convert empty strings to NaN
+df['cleaned_text'] = df['cleaned_text'].replace('', np.nan)
+
+# Drop rows with NaN
+initial_count = len(df)
+df.dropna(subset=['cleaned_text'], inplace=True)
+print(f"Removed {initial_count - len(df)} empty tweets")
+# ----------------------------
 
 #only need to determine between clean and hate speech, so combine offensive with clean for this purpose
 df['label'] = df['class'].apply(lambda x: 1 if x == 0 else 0)
